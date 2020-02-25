cc7f5356ea2c4b25531f421e088bf34aed1a0744
Girish Srinivasa <gs22@hotmail.com>
2020-02-26 06:39:15 +1100

    changes

---
+++ SEIS.Identity.Api/Models/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEIS.Identity.Api.Models
+{
+    public class Login
+    {
+    }
+}
---
+++ SEIS.Identity.Api/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEIS.Identity.Api.Models
+{
+    /// <summary>
+    /// User class holding the login credentials and other details
+    /// </summary>
+    public class User
+    {
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string LoginName { get; set; }
+        public string LoginPassword { get; set; }
+    }
+}
