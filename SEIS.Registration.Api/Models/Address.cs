bfe5b860995c2fab4fe065836f6ab87b8b48a110
Girish Srinivasa <gs22@hotmail.com>
2020-02-26 06:26:18 +1100

    Add project files.

---
+++ SEIS.Registration.Api/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEIS.Registration.Api.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public int StudentId { get; set; }
+   
+    }
+}
---
+++ SEIS.Registration.Api/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEIS.Registration.Api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string MobileNumber { get; set; }
+        public Address AddressInfo { get; set; }
+    }
+
+}
