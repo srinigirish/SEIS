4c94ea37eff3c5fb199a3eabdad2d91817743a48
Girish Srinivasa <gs22@hotmail.com>
2020-03-21 20:25:42 +1100

    changes

---
+++ SEIS.Registration.Api/Models/StudentContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SEIS.Registration.Api.Models
+{
+    /// <summary>
+    /// StudentDb Context class using EF
+    /// </summary>
+    public class StudentContext :DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+          : base(options) { }
+
+        // The set of Students 
+        public DbSet<Student> Students { get; set; }
+        // The set of Enrolled Courses 
+        public DbSet<EnrolledCourses> EnrolledCourse { get; set; }
+        // The set of Addresses
+        public DbSet<Address> Addresses { get; set; }
+
+    }
+}
